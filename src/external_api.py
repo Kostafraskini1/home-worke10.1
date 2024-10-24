df63ea0b2dad39b7117b47663e42386020be83f0
Konstantin Moiseev <volaton@gmail.com>
2024-10-24 14:26:29 +0300

    homework 12_1 done

--- src/external_api.py
+++ src/external_api.py
@@ -1,4 +1,6 @@
 import os
+from locale import currency
+
 import requests
 from dotenv import load_dotenv
 
@@ -7,14 +9,16 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 def convert_to_rub(transaction):
+    amount = transaction["operationAmount"]["amount"]
+    currency = transaction["operationAmount"]["currency"]["code"]
     if transaction['currency'] == 'RUB':
         return float(transaction['amount'])
 
     response = requests.get(
-        f'https://api.apilayer.com/exchangerates_data/latest?base={transaction["currency"]}&symbols=RUB',
+        f'https://api.apilayer.com/exchangerates_data/convert?to={transaction["currency"]}&symbols=RUB',
         headers={'apikey': API_KEY}
     )
     response.raise_for_status()
-    rates = response.json()
+    result = response.json()
 
-    return float(transaction['amount']) * rates['rates']['RUB']
+    return float(transaction['amount']) * result['result']['RUB']
