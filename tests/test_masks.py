cdde7c3a3f757cd7a43ced95f36fc9aa92d9dd33
Konstantin Moiseev <volaton@gmail.com>
2025-03-28 17:28:15 +0300

    homework_11_1_1

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,13 +1,22 @@
 import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
-def test_get_mask_card_number():
-    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
-    assert get_mask_card_number("1234567812345678") == "1234 56** **** 5678"
-    assert get_mask_card_number("") == ""
+@pytest.mark.parametrize("card_number, expected", [
+    ("7000792289606361", "7000 79** **** 6361"),
+    ("1234567812345678", "1234 56** **** 5678"),
+    ("", ""),
+    ("1234", "1234"),
+    ("12345678901234567890", "1234 56** **** 3456"),
+])
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
 
-def test_get_mask_account():
-    assert get_mask_account("12345678901234567890") == "**7890"
-    assert get_mask_account("98765432") == "**5432"
-    assert get_mask_account("") == ""
+@pytest.mark.parametrize("account_number, expected", [
+    ("123456789012", "**9012"),
+    ("1234", "**34"),
+    ("", ""),
+    ("123456", "**56"),
+])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
 
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,28 +1,30 @@
 import pytest
 from src.processing import filter_by_state, sort_by_date
 
-list_dict = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
-def test_filter_by_state():
-    assert filter_by_state(list_dict, 'EXECUTED') == [
+@pytest.fixture
+def sample_data():
+    return [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-    ]
-    assert filter_by_state(list_dict, 'CANCELED') == [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
-    assert filter_by_state(list_dict, 'INVALID_STATE') == []
 
-def test_sort_by_date():
-    sorted_list = sort_by_date(list_dict, type_sort=False)
-    assert sorted_list[0]['date'] == '2019-07-03T18:35:29.512364'  # Latest date first
+def test_filter_by_state(sample_data):
+    assert filter_by_state(sample_data, 'EXECUTED') == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
+    assert filter_by_state(sample_data, 'CANCELED') == [
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+    assert filter_by_state(sample_data, 'NON_EXISTENT') == []
 
-    sorted_list = sort_by_date(list_dict, type_sort=True)
-    assert sorted_list[0]['date'] == '2018-06-30T02:08:58.425572'  # Earliest date first
-
+@pytest.mark.parametrize("type_sort, expected_order", [
+    (True, [41428829, 939719570, 594226727, 615064591]),
+    (False, [615064591, 594226727, 939719570, 41428829]),
+])
+def test_sort_by_date(sample_data, type_sort, expected_order):
+    sorted_data = sort_by_date(sample_data, type_sort)
+    assert [item['id'] for item in sorted_data] == expected_order
